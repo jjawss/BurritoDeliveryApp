5dd25953582b1eb34624d48003ad6354f6e566af
kareemiztolive <nelsonsmf@gmail.com>
2019-12-03 16:10:48 -0600

    sign up form created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,25 @@ end
 
 def create
 
+    user = User.find(:params[:user_name])
+
+    if !user
+
+    User.create({
+        name: params[:name]
+        address: params[:address]
+        username: params[:username]
+        password: params[:password]
+    })
+
+    else
+    
+        "That Username Already exist"
+    
+        # flash[:user_name_exist] = "That Username Already exist"
+    
+    end
+
 
 
 end
