22cd3d54039b75cf6533e203a59ed9c0c13707da
David Jaworski <48140376+jjawss@users.noreply.github.com>
2019-12-03 18:38:01 -0600

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,29 @@
 def new
-
-
+    @user = User.new
+    @error_message = flash[:user_name_exist]
 end
 
 def create
 
-    user = User.find(:params[:user_name])
+    user = User.find_by(username: params[:user][:username])
 
     if !user
 
     User.create({
-        name: params[:name]
-        address: params[:address]
-        username: params[:username]
+        name: params[:name],
+        address: params[:address],
+        username: params[:username],
         password: params[:password]
     })
 
     else
     
-        "That Username Already exist"
+        #"That Username Already exist"
     
-        # flash[:user_name_exist] = "That Username Already exist"
-    
+        flash[:user_name_exist] = "That Username Already exist"
+        redirect_to "/users/new"
     end
 
-
+end
 
 end
